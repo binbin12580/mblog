71cd0b53688a6e1985c7e90546d30274665ac7df
langhsu <langhsu@163.com>
2017-10-12 14:11:41 +0800

    bug fixed

--- mblog-web/src/main/java/mblog/web/controller/api/SidebarController.java
+++ mblog-web/src/main/java/mblog/web/controller/api/SidebarController.java
@@ -28,8 +28,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.plugin.util.UserProfile;
-
 import java.util.List;
 
 /**
