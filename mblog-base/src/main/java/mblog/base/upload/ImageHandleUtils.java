dcb4d11b1a78dd50105cd543af382df6929ddbb8
langhsu <langhsu@163.com>
2017-10-13 12:21:13 +0800

    配置调整 图片压缩 默认为 Thumb

--- mblog-base/src/main/java/mblog/base/upload/ImageHandleUtils.java
+++ mblog-base/src/main/java/mblog/base/upload/ImageHandleUtils.java
@@ -52,6 +52,7 @@ public class ImageHandleUtils {
         } else{
             GMagickUtils.scale(ori, dest, width,height);
         }
+//        Thumbnails.of(ori).size(width, height).toFile(dest);
     }
 
     /**
